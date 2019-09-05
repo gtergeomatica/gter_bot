43fac9b1bcc16da6d417f6727d6a65f10631035e
Roberto Marzocchi <roberto.marzocchi@gter.it>
2019-09-05 16:46:49 +0200

    Passi avanti nell'async

--- test_gter_bot_multithread.py
+++ test_gter_bot_multithread.py
@@ -3,13 +3,23 @@
 
 # Roberto Marzocchi copyleft 2019
 
+import asyncio
+
+# da togliere
+import random
+
 
 import telepot
-from telepot.loop import MessageLoop
+
+#python 3
+from telepot.aio.loop import MessageLoop
+#from telepot.aio.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
+from telepot.aio.delegate import pave_event_space, per_chat_id, create_open, per_callback_query_origin
+#python2 
+#from telepot.loop import MessageLoop
 #questo per i tastini
 from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
-
-from telepot.delegate import pave_event_space, per_chat_id, create_open
+#from telepot.delegate import pave_event_space, per_chat_id, create_open
 
 
 from pprint import pprint
@@ -28,14 +38,21 @@ import config
 
 # Il token è contenuto nel file config.py e non è aggiornato su GitHub per evitare utilizzi impropri
 TOKEN=config.TOKEN
- 
-class MessageCounter(telepot.helper.ChatHandler):
+
+
+
+
+# questa classe usa il ChatHandler telepot.aio.helper.ChatHandler (ossia è in ascolto della chat del BOT)
+class MessageCounter(telepot.aio.helper.ChatHandler):
     def __init__(self, *args, **kwargs):
         super(MessageCounter, self).__init__(*args, **kwargs)
         self._count = 0
 
-    def on_chat_message(msg):
-        content_type, chat_type, chat_id = telepot.glance(msg) #get dei parametri della conversazione e del tipo di messaggio
+    async def on_chat_message(self, msg):
+        #contatore messaggi
+        self._count += 1
+        content_type, chat_type, chat_id = telepot.glance(msg)
+        #content_type, chat_type, chat_id = telepot.glance(msg) #get dei parametri della conversazione e del tipo di messaggio
         command = msg['text'] #get del comando inviato
         try:
             nome = msg["from"]["first_name"]
@@ -47,34 +64,81 @@ class MessageCounter(telepot.helper.ChatHandler):
             cognome= ""
         is_bot = msg["from"]["is_bot"]
         if is_bot=='True':
-            bot.sendMessage(chat_id, "ERROR: questo Bot non risponde ad altri bot!")
+            #bot.sendMessage(chat_id, "ERROR: questo Bot non risponde ad altri bot!")
+            await self.sender.sendMessage("ERROR: questo Bot non risponde ad altri bot!")
         elif command == '/myip':
             my_ip = urlopen('http://ip.42.pl/raw').read() #get dell'IP
-            bot.sendMessage(chat_id, my_ip)
+            #bot.sendMessage(chat_id, my_ip)
+            message = "Messaggio {} - Gentile {} {}, l'indirizzo IP del server che ti sta rispondendo è {}".format(self._count, nome, cognome,my_ip)
+            await self.sender.sendMessage(message)
         elif command == '/getinfo':
-            info=json.dumps(bot.getUpdates(),sort_keys=True, indent=4) #get delle info e tramite json.dump formattiamo il messaggio 
-            bot.sendMessage(chat_id,info)
+            await self.sender.sendMessage(
+                'Press START to do some math ...',
+                reply_markup=InlineKeyboardMarkup(
+                    inline_keyboard=[[
+                        InlineKeyboardButton(text='START', callback_data='start'),
+                    ]]
+                )
+            )
+            #info=json.dumps(bot.getUpdates(),sort_keys=True, indent=4) #get delle info e tramite json.dump formattiamo il messaggio 
+            #message = "Messaggio {} - Gentile {} {}, ecco il getinfo del tuo ultimo messaaggio {}".format(self._count, nome, cognome,info)
+            #bot.sendMessage(chat_id,info)
+            #await self.sender.sendMessage(message)
         elif command == '/chatid':
-            message = "Gentile {0} {1} il tuo codice da inserire nell'applicazione è {2}".format(nome, cognome, chat_id)
-            bot.sendMessage(chat_id,message)
+            message = "Messaggio {0} - Gentile {1} {2} il tuo codice da inserire nell'applicazione è {3}".format(self._count,nome, cognome, chat_id)
+            #bot.sendMessage(chat_id,message)
+            await self.sender.sendMessage(message)
         elif command == '/sito_gter':
-            bot.sendMessage(chat_id,"https://www.gter.it")
+            message = "Messaggio {0} - Gentile {1} {2} il sito web di Gter è https://www.gter.it Cliccaci subito prima che il bot autodistrugga il tuo telefono".format(self._count,nome, cognome, chat_id)
+            #bot.sendMessage(chat_id,message)
+            await self.sender.sendMessage(message)
+            #bot.sendMessage(chat_id,"https://www.gter.it")
         else:
-            content_type, chat_type, chat_id = telepot.glance(msg)
-            keyboard = InlineKeyboardMarkup(inline_keyboard=[
-                         [InlineKeyboardButton(text='IP del server', callback_data='ip')],
-                         [InlineKeyboardButton(text='Sito Gter', callback_data='info')],
-                         [InlineKeyboardButton(text='Demo Comunicazione', callback_data='demo_com')],
-                         [InlineKeyboardButton(text='Chat ID', callback_data='chat_id')],
-                         [InlineKeyboardButton(text='Time', callback_data='time')],
-                     ])
-            bot.sendMessage(chat_id, 'Gentile {0} {1} questo è un bot configurato per alcune operazioni minimali, quanto hai scritto non è riconosciuto, invece di fotterti prova con i seguenti tasti:'.format(nome,cognome), reply_markup=keyboard)
-        
-        
-        
-    def on_callback_query(msg):
-        query_id, chat_id, query_data = telepot.glance(msg, flavor='callback_query')
-        print('Callback Query:', query_id, chat_id, query_data)
+                keyboard = InlineKeyboardMarkup(inline_keyboard=[
+                             [InlineKeyboardButton(text='IP del server', callback_data='ip')],
+                             [InlineKeyboardButton(text='START', callback_data='start')],
+                             #[InlineKeyboardButton(text='Sito Gter', callback_data='info')],
+                             #[InlineKeyboardButton(text='Demo Comunicazione', callback_data='demo_com')],
+                             #[InlineKeyboardButton(text='Chat ID', callback_data='chat_id')],
+                             #[InlineKeyboardButton(text='Time', callback_data='time')],
+                         ])
+                #bot.sendMessage(chat_id, 'Gentile {0} {1} questo è un bot configurato per alcune operazioni minimali, quanto hai scritto non è riconosciuto, invece di fotterti prova con i seguenti tasti:'.format(nome,cognome), reply_markup=keyboard)
+                message = "Messaggio {} - Gentile {} {}, questo è un bot configurato per alcune operazioni minimali, quanto hai scritto non è riconosciuto, invece di fotterti prova con i seguenti tasti:".format(self._count, nome, cognome)
+                await self.sender.sendMessage(message, reply_markup=keyboard)
+
+
+# questa classe usa il CallbackQueryOriginHandler telepot.aio.helper.CallbackQueryOriginHandler (ossia è in ascolto dei tasti schoacchiati dal BOT)
+class Quizzer(telepot.aio.helper.CallbackQueryOriginHandler):
+    def __init__(self, *args, **kwargs):
+        super(Quizzer, self).__init__(*args, **kwargs)
+        self._score = {True: 0, False: 0}
+        self._answer = None
+        self._messaggio = ''
+
+    async def _show_next_question(self):
+        x = random.randint(1,50)
+        y = random.randint(1,50)
+        sign, op = random.choice([('+', lambda a,b: a+b),
+                                  ('-', lambda a,b: a-b),
+                                  ('x', lambda a,b: a*b)])
+        answer = op(x,y)
+        question = '%d %s %d = ?' % (x, sign, y)
+        choices = sorted(list(map(random.randint, [-49]*4, [2500]*4)) + [answer])
+
+        await self.editor.editMessageText(question,
+            reply_markup=InlineKeyboardMarkup(
+                inline_keyboard=[
+                    list(map(lambda c: InlineKeyboardButton(text=str(c), callback_data=str(c)), choices))
+                ]
+            )
+        )
+        return answer
+
+    async def on_callback_query(self, msg):
+        query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
+        #content_type, chat_type, chat_id = telepot.glance(msg)
+        #parte copiata
+        print('Callback Query:', query_id, query_data)
         try:
             command = msg['text'] #get del comando inviato
         except:
@@ -88,35 +152,33 @@ class MessageCounter(telepot.helper.ChatHandler):
         except:
             cognome= ""
         is_bot = msg["from"]["is_bot"]
-        if is_bot=='True':
-            bot.sendMessage(chat_id, "ERROR: questo Bot non risponde ad altri bot!")
-        elif query_data=='ip':
+        if query_data=='ip':
             my_ip = urlopen('http://ip.42.pl/raw').read()
-            message = "Gentile {0} {1} il BOT ti sta rispondendo da un server con il segunete indirizzo IP {2}".format(nome, cognome, my_ip)
-            bot.sendMessage(chat_id, message) 
-        elif query_data=='info':
-            info=json.dumps(bot.getUpdates(),sort_keys=True, indent=4)
-            message = "Gentile {0} {1} ecco il sito web di Gter srl: {2}".format(nome, cognome, "https://www.gter.it")
-            bot.sendMessage(chat_id, message) 
-        elif query_data=='chat_id':
-            message = "Gentile {0} {1} il tuo codice da inserire nell'applicazione è {2}".format(nome, cognome, chat_id)
-            bot.sendMessage(chat_id, message) 
-        elif query_data=='demo_com':
-            message = "Gentile {0} {1} hai scelto di inserire una nuova comunicazione{2}".format(nome, cognome, chat_id)        
+            message = "Gentile {} {}, l'indirizzo IP del server che ti sta rispondendo è {}".format(nome, cognome,my_ip)
+            print(message)
             #bot.sendMessage(chat_id, message)
-            
-        elif query_data=='time':
-            ts = time.time()
-            message = "Gentile {0} {1} sono le {2}".format(nome, cognome, datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S'))
-            bot.sendMessage(chat_id, message) 
-            #bot.answerCallbackQuery(query_id, text=datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')) #messaggio a comparsa
-        elif command == '/getinfo':
-            info=json.dumps(bot.getUpdates(),sort_keys=True, indent=4) #get delle info e tramite json.dump formattiamo il messaggio 
-            message = "Gentile {0} {1} ecco le info inviate {2}".format(nome, cognome, info)
-            bot.sendMessage(chat_id, message) 
-        #else: 
-            #message = "Gentile {0} {1} il tasto che hai schiacciato non funziona ancora, a questo punto pare necessario che tu ti fotta".format(nome, cognome)
-        #bot.sendMessage(chat_id, message) 
+            #await self.sender.sendMessage(message)
+        elif query_data != 'start':
+            print('ora ho capito cosa succede qua')
+            self._score[self._answer == int(query_data)] += 1
+        elif query_data == 'start':
+            print('ho effettivamente schiacciato il bottone start')
+            self._answer = await self._show_next_question()
+
+    async def on__idle(self, event):
+        text = '%d out of %d' % (self._score[True], self._score[True]+self._score[False])
+        await self.editor.editMessageText(
+            text + '\n\nThis message will disappear in 5 seconds to test deleteMessage',
+            reply_markup=None)
+
+        await asyncio.sleep(5)
+        #await self.editor.deleteMessage()
+        self.close()
+
+
+
+
+
 
 
 
@@ -124,14 +186,20 @@ class MessageCounter(telepot.helper.ChatHandler):
 
 
 # questo è il "main" del BOT che è in ascolto 
-bot = telepot.DelegatorBot(TOKEN, [
+bot = telepot.aio.DelegatorBot(TOKEN, [
+    #chat
     pave_event_space()(
-        per_chat_id(), create_open, MessageCounter, timeout=10),
+        per_chat_id(), create_open, MessageCounter, timeout=120),
+    # bottoni    
+    pave_event_space()(
+        per_callback_query_origin(), create_open, Quizzer, timeout=10),
 ])
-MessageLoop(bot).run_as_thread()
 
-while 1:
-    time.sleep(10)
+loop = asyncio.get_event_loop()
+loop.create_task(MessageLoop(bot).run_forever())
+print('Listening ...')
+
+loop.run_forever()
 
 
 
